f747a6f4fe6b7cbe50adccd4abf8af8e96b0a015
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-09-09 15:34:36 -0400

    V1

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 void setup()
 {
   pinMode(MA_BROCHE_BOUTON, INPUT);
+  Serial.begin(115200);
 }
 
 void loop()
 {
   int maLectureBouton = digitalRead(MA_BROCHE_BOUTON);
+  maLectureBouton = analogRead(MA_BROCHE_BOUTON);
+  Serial.println(MA_BROCHE_BOUTON);
 }
