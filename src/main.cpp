29bf94758789a48ef748eddbb0b8c1eafc4b0346
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-09-09 15:22:11 -0400

    Ajout #Define, pinmode et digiitalread

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#define MA_BROCHE_BOUTON 39
 
 void setup()
 {
+  pinMode(MA_BROCHE_BOUTON, INPUT);
 }
 
 void loop()
 {
+  int maLectureBouton = digitalRead(MA_BROCHE_BOUTON);
 }
